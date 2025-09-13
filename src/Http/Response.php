f700e4c3e18de3f67a607ad1c365efe0283f0359
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-09-14 00:35:42 +0700

    Changed response class.

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -4,19 +4,40 @@ namespace Annabel\Http;
 
 class Response
 {
-    public function __construct(
-        protected string $content,
-        protected int $status = 200,
-        protected array $headers = []
-    ) {
+    protected string $content;
+    protected int $status;
+    protected array $headers = [];
+
+    public function __construct(string $content = '', int $status = 200, array $headers = [])
+    {
+        $this->content = $content;
+        $this->status = $status;
+        $this->headers = $headers;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function getStatusCode(): int
+    {
+        return $this->status;
+    }
+
+    public function getHeaders(): array
+    {
+        return $this->headers;
     }
 
     public function send(): void
     {
         http_response_code($this->status);
-        foreach ($this->headers as $key => $value) {
-            header("$key: $value");
+
+        foreach ($this->headers as $name => $value) {
+            header("$name: $value");
         }
+
         echo $this->content;
     }
 }
